894733e6c86dade6befbdd59f871b9c72013edc6
developer-suhag <developersuhag@gmail.com>
2021-09-22 23:35:51 +0600

    logo menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
 import logo from "../../images/logo.png";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h2>this is header</h2>
-      <img src={logo} alt="" />
+    <div className="header">
+      <img className="logo" src={logo} alt="" />
+      <nav className="nav-bar">
+        <a href="/shop">Shop</a>
+        <a href="/orders">Order Review</a>
+        <a href="/inventory">Manage Inventroy Here</a>
+      </nav>
+      <div className="serach-cart">
+        <input type="search" name="" id="" placeholder="type here to search" />
+        <span></span>
+      </div>
     </div>
   );
 };
